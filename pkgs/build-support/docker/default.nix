35e5cfa050ac381f6bd9b80e550b2b0ab0dcaeb1
Yovko Lambrev <yovko@yovko.net>
2025-10-22 10:05:18 +0300

    Eliminate strange gcroots symlinks creation
    the gcroots stuff moved in front of our change

--- pkgs/build-support/docker/default.nix
+++ pkgs/build-support/docker/default.nix
@@ -76,6 +76,10 @@ let
       ${buildPackages.nix}/bin/nix-store --load-db < ${
         closureInfo { rootPaths = contentsList; }
       }/registration
+      mkdir -p nix/var/nix/gcroots/docker/
+      for i in ${lib.concatStringsSep " " contentsList}; do
+      ln -s $i nix/var/nix/gcroots/docker/$(basename $i)
+      done;
       ${lib.optionalString includeNixDBHostSignatures ''
         # copy signatures from building system (-s "local?read-only=true") into "NIX_REMOTE=local?root=$PWD"
         # readonly store is required to avoid write operations which might fail due to missing privileges
@@ -83,11 +87,6 @@ let
       ''}
       # Reset registration times to make the image reproducible
       ${buildPackages.sqlite}/bin/sqlite3 nix/var/nix/db/db.sqlite "UPDATE ValidPaths SET registrationTime = ''${SOURCE_DATE_EPOCH}"
-
-      mkdir -p nix/var/nix/gcroots/docker/
-      for i in ${lib.concatStringsSep " " contentsList}; do
-      ln -s $i nix/var/nix/gcroots/docker/$(basename $i)
-      done;
     '';
 
   # The OCI Image specification recommends that configurations use values listed
